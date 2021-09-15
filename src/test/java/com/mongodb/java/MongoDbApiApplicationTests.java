ff8442eb46ec66bd967a0f1d7185072499c72b37
Marcio Grecco <marcio.greco@outlook.com>
2021-09-15 16:30:37 -0300

    Project Created

---
+++ src/test/java/com/mongodb/java/MongoDbApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mongodb.java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongoDbApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
